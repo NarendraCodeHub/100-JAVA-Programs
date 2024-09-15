eee873d84b0c3ca70b425e91a0f01a9f2d9ed04e
Narendra122812 <narendrakumar122812@gmail.com>
2024-09-15 23:12:09 +0530

    Added Integers.java

---
+++ src/Integers.java
@@ -0,0 +1,16 @@
+
+/**
+ * 2.Print Integer in java
+ */
+
+public class Integers {
+
+	public static void main(String[] args) {
+		
+		//Print Integers through by using for Loop
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
