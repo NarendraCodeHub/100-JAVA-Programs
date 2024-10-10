30208dda1bcdec49f0c614989b386459c5339ec6
Narendra122812 <narendrakumar122812@gmail.com>
2024-10-10 22:20:15 +0530

    Added GetInputFromUser.java File

---
+++ src/GetInputFromUser.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GetInputFromUser {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the A Number : ");
+		int number = input.nextInt();
+		System.out.println("You entered integer "+ number);
+		
+		System.out.println("Enter a Decimal Number : ");
+		double decimal_num = input.nextDouble();
+		System.out.println("You entered Decimal Number "+ decimal_num);
+		
+		System.out.println("Enter the String : ");
+		String word = input.next();
+		System.out.println("You entered string "+word);
+	}
+
+}
