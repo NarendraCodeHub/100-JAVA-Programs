1aa80de78e39cefce7e3e4d460124fcf4e83364f
Narendra122812 <narendrakumar122812@gmail.com>
2024-10-23 00:05:23 +0530

    Added OddOrEven.ajav File

---
+++ src/OddOrEven.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * 12. How to check Odd and Even Number in java.
+ */
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	try {
+		System.out.println("Enter the numbers: ");
+		int  num = scan.nextInt();
+		if (num>=1) {
+			if (num%2==0) {
+				System.out.println(num +" is Even Number.");
+			}else {
+				System.out.println(num +" is Odd Number.");
+			}
+		}else {
+			System.out.println("Enter a Positive Number");
+		}
+		
+	}catch (Exception e) {
+			System.out.println("Invalid Input !!!");
+		}
+	}
+}
