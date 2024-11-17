614c53b6f3ce2e4234cd77eef79df5d7cea844fa
Narendra122812 <narendrakumar122812@gmail.com>
2024-11-17 16:43:33 +0530

    Added Factorial.java File

---
+++ src/Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/**
+ * 13. Find factorial for given no Program in Java
+ */
+public class Factorial {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println("Enter the Factorail Number: ");
+			int num = scan.nextInt();
+			if (num>=1) {
+				int temp = 1;
+				for (int i = num; i >= 1; i--) {
+					temp = temp * i;
+				}
+				System.out.println("Factorial Number of "+ num +" is "+ temp);
+			}else {
+				System.out.println("The factorial of a number starts with an integer greater than or equal to one.");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Invalid Input !!!");	
+			}
+	}
+}
