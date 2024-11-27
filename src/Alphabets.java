8f4f7a3969c2b13bb6aac591b2b7a1cdd9b558ed
Narendra122812 <narendrakumar122812@gmail.com>
2024-11-27 23:32:23 +0530

    Added Alphabets.java File

---
+++ src/Alphabets.java
@@ -0,0 +1,17 @@
+
+/**
+ * 19. Print all alphabet using for loop Program in 
+ */
+public class Alphabets {
+
+	public static void main(String[] args) {
+		for (char i = 'A'; i <='Z'; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println("\n");
+		for (char j = 'a'; j <= 'z'; j++) {
+			System.out.print(j + " ");
+		}
+	}
+
+}
