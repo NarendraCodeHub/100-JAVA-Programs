3b95658fe70884f952b370d748858efd99baef76
Narendra122812 <narendrakumar122812@gmail.com>
2024-12-01 21:05:31 +0530

    Added PrimeNumbers.java

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,14 +1,47 @@
 import java.util.Scanner;
-
+/*
+ * 22.Print prime no Program in java
+ */
 public class PrimeNumbers {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            // Input validation for starting number
+            System.out.println("Enter the Starting Number: ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Invalid Input! Enter a valid number:");
+                scan.next();
+            }
+            int start_num = scan.nextInt();
 
-	public static void main(String[] args) {
-		try (Scanner scan = new Scanner(System.in)){
-			System.out.println("Enter the Starting Number: ");
-			int start_num = scan.nextInt()
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	}
+            // Input validation for ending number
+            System.out.println("Enter the Ending Number: ");
+            while (!scan.hasNextInt()) {
+                System.out.println("Invalid Input! Enter a valid number:");
+                scan.next();
+            }
+            int end_num = scan.nextInt();
 
+            // Checking if input numbers are greater than 1 and in correct range
+            if (start_num > 1 && end_num > 1 && end_num >= start_num) {
+                System.out.println("Prime numbers between " + start_num + " and " + end_num + " are:");
+                
+                for (int i = start_num; i <= end_num; i++) {
+                    boolean isPrime = true;
+                    for (int j = 2; j <= Math.sqrt(i); j++) { 
+                        if (i % j == 0) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime) {
+                        System.out.println(i + " is a Prime Number.");
+                    }
+                }
+            } else {
+                System.out.println("Starting and ending numbers must be greater than 1, and the ending number must be greater than or equal to the starting number.");
+            }
+        } catch (Exception e) {
+            System.out.println("Unexpected error! Please try again.");
+        }
+    }
 }
