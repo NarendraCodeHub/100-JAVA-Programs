c7d8be0f401d3239b287824029f2cb9733e64986
Narendra122812 <narendrakumar122812@gmail.com>
2024-12-25 23:25:55 +0530

    SubstringsOfAString.java

--- src/SubstringsOfAString.java
+++ src/SubstringsOfAString.java
@@ -1,10 +1,28 @@
-
 /*
  * 24. Find All substring of string in java Program
  */
-public class SubstringsOfAString {
 
+import java.util.Scanner;
+
+public class SubstringsOfAString {
 	public static void main(String[] args) {
+
+		try (Scanner scan = new Scanner(System.in)) {
+			String string, sub;
+			System.out.println("Enter a string to print it's all substrings");
+			string = scan.nextLine();
+			int length = string.length();
+			System.out.println("Substrings of \"" + string + "\" are :-");
+			for (int c = 0; c < length; c++) {
+				for (int i = 1; i <= length - c; i++) {
+					sub = string.substring(c, c + i);
+					System.out.println(sub);
+				}
+				System.out.println("------------------");
+			}
+		} catch (Exception e) {
+			System.out.println("Unexpected error! Please try again.");
+		}
 
 	}
 
