d2c10d01c073a80b65e2348d882d3d61226f636e
Narendra122812 <narendrakumar122812@gmail.com>
2026-05-16 15:09:46 +0530

    Modify Difference.java Program

--- src/Difference.java
+++ src/Difference.java
@@ -1,7 +1,18 @@
-
+/**
+ * 37.Difference between Static and Instance method working in java Program
+ */
 public class Difference {
 	public static void main(String[] args) {
-		System.out.println("Tesing");
+		display(); // Calling without Object
+		Difference object = new Difference();
+		object.show(); // Calling Using Object
+	}
 
+	static void display() {
+		System.out.println("This is Static Method");
+	}
+
+	void show() {
+		System.out.println("THis is Non-Static Method");
 	}
 }
